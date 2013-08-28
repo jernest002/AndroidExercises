7721d6d55d001b9046ec08c2fd9841c14796ce25
John Ernest Guadalupe <j.guadalupe@klab.com>
2013-08-28 12:59:43 +0800

    Add CriminalIntent App for Exercise Applications

--- src/com/example/androidtraining/ExerciseAppsActivity.java
+++ src/com/example/androidtraining/ExerciseAppsActivity.java
@@ -10,12 +10,21 @@ import android.view.View;
 import android.widget.Button;
 
 public class ExerciseAppsActivity extends Activity {
+	private Button mExer1_3;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_exercise_apps);
 		
+		mExer1_3 = (Button) findViewById(R.id.exer1_3_button);
+		mExer1_3.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(ExerciseAppsActivity.this, CrimeListActivity.class);
+				startActivity(i);
+			}
+		});
 	}
 
 	@Override
