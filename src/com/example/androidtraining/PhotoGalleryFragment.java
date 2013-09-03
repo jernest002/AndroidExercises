11b271b041af8c39d47961cd9197899879ef0947
John Ernest Guadalupe <j.guadalupe@klab.com>
2013-09-03 11:26:20 +0800

    Add setupAdapter method in PhotoGalleryFragment to display an ArrayList of GalleryItem objects

--- src/com/example/androidtraining/PhotoGalleryFragment.java
+++ src/com/example/androidtraining/PhotoGalleryFragment.java
@@ -1,20 +1,21 @@
 package com.example.androidtraining;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 public class PhotoGalleryFragment extends Fragment {
 	private static final String TAG = "PhotoGalleryFragment";
 	
 	GridView mGridView;
+	ArrayList<GalleryItem> mItems;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -29,8 +30,19 @@ public class PhotoGalleryFragment extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
 		
 		mGridView = (GridView)v.findViewById(R.id.gridView);
-		
+		setupAdapter();
 		return v;
+	}
+	
+	void setupAdapter() {
+		if (getActivity() == null || mGridView == null) return;
+		
+		if (mItems != null) {
+			mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(),
+					android.R.layout.simple_gallery_item, mItems));
+		} else {
+			mGridView.setAdapter(null);
+		}
 	}
 	
 	class FetchItemsTask extends AsyncTask<Void,Void,Void>{
