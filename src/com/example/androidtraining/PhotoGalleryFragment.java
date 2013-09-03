4c574a3e43b125b3399e39664c75099a469f1c94
John Ernest Guadalupe <j.guadalupe@klab.com>
2013-09-03 10:10:48 +0800

    add FetchItemsTask in PhotoGalleryFragment

--- src/com/example/androidtraining/PhotoGalleryFragment.java
+++ src/com/example/androidtraining/PhotoGalleryFragment.java
@@ -1,13 +1,19 @@
 package com.example.androidtraining;
 
+import java.io.IOException;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
 public class PhotoGalleryFragment extends Fragment {
+	private static final String TAG = "PhotoGalleryFragment";
+	
 	GridView mGridView;
 	
 	@Override
@@ -24,5 +30,20 @@ public class PhotoGalleryFragment extends Fragment {
 		mGridView = (GridView)v.findViewById(R.id.gridView);
 		
 		return v;
+	}
+	
+	class FetchItemsTask extends AsyncTask<Void,Void,Void>{
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+					String result = new FlickrFetchr().getUrl("http://www.google.com");
+					Log.i(TAG, "Fetched contents of URL: " + result);
+				} catch (IOException ioe) {
+					Log.e(TAG, "Failed to fetch URL: ", ioe);
+				}
+			return null;
+		}
+		
 	}
 }
