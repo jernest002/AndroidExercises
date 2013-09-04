f909e4ea58dd9f9bb44c5178a763d86a040a8985
John Ernest Guadalupe <j.guadalupe@klab.com>
2013-09-04 11:07:26 +0800

    Add photo page function to PhotoGalleryApp

--- src/com/example/androidtraining/PhotoGalleryFragment.java
+++ src/com/example/androidtraining/PhotoGalleryFragment.java
@@ -8,13 +8,14 @@ import android.app.SearchManager;
 import android.app.SearchableInfo;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -22,6 +23,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -66,6 +69,18 @@ public class PhotoGalleryFragment extends VisibleFragment {
 		
 		mGridView = (GridView)v.findViewById(R.id.gridView);
 		setupAdapter();
+		
+		mGridView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> gridView, View view, int pos,
+				long id) {
+					GalleryItem item = mItems.get(pos);
+					Uri photoPageUri = Uri.parse(item.getPhotoPageUrl());
+					Intent i = new Intent(Intent.ACTION_VIEW, photoPageUri);
+					startActivity(i);
+			}
+		});
+		
 		return v;
 	}
 	
@@ -184,17 +199,19 @@ public class PhotoGalleryFragment extends VisibleFragment {
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			if (convertView == null) {
-			convertView = getActivity().getLayoutInflater()
-			.inflate(R.layout.gallery_item, parent, false);
-		}
+				convertView = getActivity().getLayoutInflater()
+						.inflate(R.layout.gallery_item, parent, false);
+			}
 			
-		ImageView imageView = (ImageView)convertView
-		.findViewById(R.id.gallery_item_imageView);
-		imageView.setImageResource(R.drawable.brian_up_close);
-		GalleryItem item = getItem(position);
-		mThumbnailThread.queueThumbnail(imageView, item.getUrl());
-		
-		return convertView;
+			ImageView imageView = (ImageView)convertView
+			.findViewById(R.id.gallery_item_imageView);
+			
+			imageView.setImageResource(R.drawable.brian_up_close);
+			
+			GalleryItem item = getItem(position);
+			mThumbnailThread.queueThumbnail(imageView, item.getUrl());
+			
+			return convertView;
 		}
 	}
 }
